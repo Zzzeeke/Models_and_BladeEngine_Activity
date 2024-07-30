7d0f0bb1018ea9f740b6eb51651689444bbfa389
Zzzeeke <zekecagas@gmail.com>
2024-07-31 01:36:03 +0800

    added backend codes for footer, header, and web

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,5 @@
+<footer>
+    <div class="container mx-auto text-center">
+        <p>&copy; {{ date('Y') }} FakeBooks Eyy</p>
+    </div>
+</footer>
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,5 @@
+<header>
+    <div class="container mx-auto">
+        <h1 class="text-2xl">FakeBooks</h1>
+    </div>
+</header>
